f40fe7ed4a21d019db204ccb26c8e652e4794c0d
alepri <alessandro.lepri@selfridges.co.uk>
2020-01-29 21:45:19 +0000

    All files created

---
+++ app.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './style.scss'
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render( <
+    App / > ,
+    document.getElementById('root')
+)
