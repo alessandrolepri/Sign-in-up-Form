e82b88293587fda8bafdcd40f235565c2e876fe9
alepri <alessandro.lepri@selfridges.co.uk>
2020-01-29 23:02:11 +0000

    Template created

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom'
 
 import './style.scss'
 
+import Form from './component/form'
 
 
-
-
-
-
-
-
-
-
+class App extends React.Component {
+    render() {
+        return ( <
+            Form / >
+        )
+    }
+}
 
 ReactDOM.render( <
     App / > ,
--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,26 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve('dist'),
         publicPath: '/'
     },
     module: {
-        rules: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-            { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] }
+        rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                loader: ['style-loader', 'css-loader', 'sass-loader']
+            }
         ]
     },
     devServer: {
@@ -27,7 +36,7 @@ module.exports = {
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
-            template: 'src/index.html',
+            template: 'index.html',
             filename: 'index.html',
             inject: 'body'
         })
